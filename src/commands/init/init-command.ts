2bd47b5003b3b8b0c98a3c368685d3b6528bb88a
Vladimir Belov <i@vovkabelov.ru>
2026-02-25 19:45:21 +0200

    Update init command messages and warnings

--- src/commands/init/init-command.ts
+++ src/commands/init/init-command.ts
@@ -1,17 +1,16 @@
-import { Command } from 'commander';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
+import { Command } from 'commander';
 import ora from 'ora';
 import chalk from 'chalk';
 import boxen from 'boxen';
-import { confirm } from '@inquirer/prompts';
+import logSymbols from 'log-symbols';
 
 import { Environment } from '../../environment/environment';
 import { pathOption } from './options/path-option';
 import { PackageFactoryProvider } from '../../modules/packages/providers/package-factory-provider';
 import { findPackages } from '../../utils/package/find-packages';
 import { TemplateService } from '../../modules/services/template/template.service';
-import { fileExistsAsync } from '../../utils/file.exists.async';
 import { safeFileWrite, SaveFileStatus } from '../../utils/safe.file.write';
 import { multiline } from '../../utils/multiline.text.tag';
 
@@ -22,7 +21,7 @@ const initTestsCommand = new Command('tests')
 	.description('Initialize tests environment')
 	.option('-f, --force', 'Force initialization')
 	.addOption(pathOption)
-	.action(async (subcommand, args) => {
+	.action(async () => {
 		console.log(
 			multiline`
 				Preparing test environment in ${Environment.getRoot()}
@@ -41,7 +40,10 @@ const initTestsCommand = new Command('tests')
 
 		const targetPlaywrightConfigPath = path.join(Environment.getRoot(), 'playwright.config.ts');
 		const playWrightConfigContent = await templateService.get('playwright.config.ts.txt');
-		const playwrightConfigStatus = await safeFileWrite(targetPlaywrightConfigPath, playWrightConfigContent);
+		const playwrightConfigStatus = await safeFileWrite({
+			filePath: targetPlaywrightConfigPath,
+			data: playWrightConfigContent,
+		});
 
 		if (playwrightConfigStatus !== SaveFileStatus.CREATED)
 		{
@@ -50,7 +52,10 @@ const initTestsCommand = new Command('tests')
 
 		const targetDotEnvTestPath = path.join(Environment.getRoot(), '.env.test');
 		const dotEnvTestContent = await templateService.get('.env.test.txt');
-		const dotEnvTestStatus = await safeFileWrite(targetDotEnvTestPath, dotEnvTestContent);
+		const dotEnvTestStatus = await safeFileWrite({
+			filePath: targetDotEnvTestPath,
+			data: dotEnvTestContent,
+		});
 
 		const results = [
 			{
@@ -95,7 +100,7 @@ const initTestsCommand = new Command('tests')
 		}, '');
 
 		const message = multiline`
-			${filesChangedText.split('\n').join('\n\t\t\t')}
+			${filesChangedText.trimEnd().split('\n').join('\n\t\t\t').replace(/\n$/, '')}
 			
 			${chalk.bold("What these files do:")}
 			  • ${chalk.cyan("playwright.config.ts")} — enables running Mocha unit tests in the browser via Playwright
@@ -130,7 +135,7 @@ const initTestsCommand = new Command('tests')
 const initTSCommand = new Command('ts')
 	.description('Initialize TypeScript environment')
 	.addOption(pathOption)
-	.action(async (subcommand, args) => {
+	.action(async () => {
 		const packageFactory = PackageFactoryProvider.create();
 		const extensionsStream: NodeJS.ReadableStream = findPackages({
 			startDirectory: Environment.getRoot(),
@@ -144,22 +149,28 @@ const initTSCommand = new Command('ts')
 			},
 		};
 
-		console.log('');
-
-		console.log(
-			multiline`
-				${chalk.blue(`Preparing TypeScript environment in ${Environment.getRoot()}`)}
-				
-				${chalk.bold(`The following files will be added/updated:`)}
-				  • ${chalk.cyan('aliases.tsconfig.json')} — Contains path aliases for all extensions
-				  • ${chalk.cyan('tsconfig.json')} — Main TypeScript config that extends aliases
-			`,
-		);
-
 		let aliasesCount = 0;
 
+		const leadMessage = multiline`
+			Preparing TypeScript environment in ${Environment.getRoot()}
+
+			${chalk.bold('The following files will be added/updated:')}
+			  • ${chalk.cyan('aliases.tsconfig.json')} — Contains path aliases for all extensions
+			  • ${chalk.cyan('tsconfig.json')} — Main TypeScript config that extends aliases
+		`;
+
+		console.log(leadMessage);
+		console.log('');
+
 		return new Promise<void>((resolve, reject) => {
-			const aliasesConfigSpinner = ora('Scanning extensions...').start();
+			console.log(chalk.bold('Preparing paths aliases'));
+
+			const aliasesConfigSpinner = ora({
+				prefixText: ' ',
+				text: 'Scanning extensions...',
+			});
+
+			aliasesConfigSpinner.start();
 
 			extensionsStream
 				.on('data', ({ extension }: { extension: BasePackage }) => {
@@ -171,7 +182,7 @@ const initTSCommand = new Command('ts')
 						tsconfig.compilerOptions.paths[extension.getName()] = [`./${relativePath}`];
 
 						aliasesCount++;
-						aliasesConfigSpinner.text = `Processed ${aliasesCount} extensions...`;
+						aliasesConfigSpinner.text = `Found ${aliasesCount} extensions`;
 					}
 				})
 				.on('done', async () => {
@@ -186,173 +197,132 @@ const initTSCommand = new Command('ts')
 						aliasesConfigSpinner.succeed(
 							`aliases.tsconfig.json generated successfully with ${aliasesCount} aliases`,
 						);
-						console.log(`  → file://${aliasesPath}\n`);
+						console.log(`  → file://${aliasesPath}\n`,);
 
-						const tsconfigPath = path.join(Environment.getRoot(), 'tsconfig.json');
-						let tsConfigCreated = false;
+						console.log(chalk.bold('Preparing tsconfig.json'));
+						const templateService = new TemplateService(
+							path.join(__dirname, 'templates'),
+						);
 
-						if (await fileExistsAsync(tsconfigPath))
-						{
-							const overwrite = await confirm({
-								message: `File "${path.basename(tsconfigPath)}" already exists. Overwrite?`,
-								default: false,
+						const tsConfigPath = path.join(Environment.getRoot(), 'tsconfig.json');
+						const tsConfigContent = templateService.get('tsconfig.json.txt');
+						const tsConfigStatus = await safeFileWrite({
+							filePath: tsConfigPath,
+							theme: {
+								prefix: `  ${chalk.green(logSymbols.success)}`,
+							},
+							data: tsConfigContent,
+						});
+
+						const results = [
+							{
+								file: 'aliases.tsconfig.json',
+								status: SaveFileStatus.CREATED,
+							},
+							{
+								file: 'tsconfig.json',
+								status: tsConfigStatus,
+							}
+						];
+
+						const statusMap = {
+							[SaveFileStatus.CREATED]: 'Files created:',
+							[SaveFileStatus.REPLACED]: 'Files replaced:',
+						};
+
+						const handledStatuses = [SaveFileStatus.CREATED, SaveFileStatus.REPLACED];
+						const filesChangedText = handledStatuses.reduce((acc, status) => {
+							const files = results.filter((result) => {
+								return result.status === status;
 							});
 
-							if (overwrite)
+							if (files.length > 0)
 							{
-								const templateContent = await fs.readFile(
-									path.join(__dirname, 'templates', 'tsconfig.json.txt'),
-									'utf-8',
-								);
-								await fs.writeFile(
-									tsconfigPath,
-									templateContent,
-								);
+								acc += chalk.bold(`${statusMap[status]}\n`);
+								for (const { file } of files)
+								{
+									if (file === 'aliases.tsconfig.json')
+									{
+										acc += `  • ${chalk.yellow(file)} — Contains ${aliasesCount} path aliases for easier imports\n`;
+									}
 
-								const tsConfigSpinner = ora(`Generating tsconfig.json`).start();
-								tsConfigSpinner.succeed(
-									`tsconfig.json generated successfully with extends by aliases.tsconfig.json`,
-								);
-								console.log(
-									`  → file://${tsconfigPath}`,
-								);
-
-								tsConfigCreated = true;
+									if (file === 'tsconfig.json')
+									{
+										acc += `  • ${chalk.yellow(file)} — Main config that extends aliases for TypeScript compilation\n`;
+									}
+								}
 							}
-							else
-							{
-								console.log(
-									chalk.yellow(`  → File "${path.basename(tsconfigPath)}" not overwritten.`),
-								);
 
-								console.log(
-									'\n' +
-									boxen(
-										'To use the generated aliases, add the following to your existing tsconfig.json:\n\n' +
-										chalk.cyan('{\n  "extends": "./aliases.tsconfig.json"\n}') + '\n\n' +
-										chalk.blue('Example:\n') +
-										chalk.grey('{\n') +
-										chalk.grey('  "extends": "./aliases.tsconfig.json",\n') +
-										chalk.grey('  "compilerOptions": {\n') +
-										chalk.grey('    // your other options...\n') +
-										chalk.grey('  }\n') +
-										chalk.grey('}\n'),
-										{
-											padding: 1,
-											borderStyle: 'round',
-											borderColor: 'yellow',
-											title: chalk.bold.yellow('Manual update required'),
-										}
-									)
-								);
+							return acc;
+						}, '');
 
-								let message = '';
+						if (tsConfigStatus === SaveFileStatus.CANCELLED)
+						{
+							const requiredMessage = multiline`
+								To use the generated aliases, add the following to your existing tsconfig.json:
+								${chalk.cyan('{')}
+								${chalk.cyan('    "extends": "./aliases.tsconfig.json"')}
+								${chalk.cyan('}')}
+								
+								${chalk.blue('Example:')}
+								${chalk.grey('{')}
+								${chalk.grey('    "extends": "./aliases.tsconfig.json",')}
+								${chalk.grey('    "compilerOptions": {')}
+								${chalk.grey('        // your other options...')}
+								${chalk.grey('    }') }
+								${chalk.grey('}')}
+							`;
 
-								message += chalk.bold('Files created:\n');
-								message +=
-									'  • ' +
-									chalk.yellow('aliases.tsconfig.json') +
-									` — Contains ${aliasesCount} path aliases for easier imports\n`;
-								message += '\n';
-
-								message += chalk.bold('What these files do:\n');
-								message +=
-									'  • ' +
-									chalk.cyan('aliases.tsconfig.json') +
-									' — Maps extension names to their source paths (e.g. "ui.buttons" → "/bitrix/js/ui/buttons")\n\n';
-
-								message += chalk.bold('Next steps:\n');
-								message += '  • Update your existing tsconfig.json to extend aliases.tsconfig.json\n';
-								message += '  • You can now import modules using aliases like: import { Button } from "ui.buttons"\n';
-								message += '  • Run TypeScript compilation: chef build\n';
-								message += '  • More info: chef build --help\n\n';
-
-								message += chalk.bold.blue(`Tip:\n`);
-								message += `These configs help TypeScript resolve imports faster and make refactoring easier.\n`;
-
-								console.log(
-									'\n' +
-									boxen(message, {
+							console.log(
+								'\n' +
+								boxen(
+									requiredMessage,
+									{
+										title: `Manual update required!`,
 										padding: 1,
 										borderStyle: 'round',
-										borderColor: 'green',
-										title: chalk.bold.green(
-											`✓ TypeScript Environment Partially Configured!`,
-										),
-									}),
-								);
+										borderColor: 'red',
+									}
+								)
+							);
+						}
 
-								resolve();
-								return;
+						const titleStatus = (() => {
+							if (
+								tsConfigStatus === SaveFileStatus.REPLACED
+								|| tsConfigStatus === SaveFileStatus.CREATED
+							)
+							{
+								return 'Successfully';
 							}
-						}
-						else
-						{
-							const templateContent = await fs.readFile(
-								path.join(__dirname, 'templates', 'tsconfig.json.txt'),
-								'utf-8',
-							);
-							await fs.writeFile(
-								tsconfigPath,
-								templateContent,
-							);
 
-							const tsConfigSpinner = ora(`Generating tsconfig.json`).start();
-							tsConfigSpinner.succeed(
-								`tsconfig.json generated successfully with extends by aliases.tsconfig.json`,
-							);
-							console.log(`  → file://${tsconfigPath}`);
+							return 'Partially';
+						})();
 
-							tsConfigCreated = true;
-						}
+						const message = multiline`
+							${filesChangedText.trimEnd().split("\n").join("\n\t\t\t\t\t\t\t").replace(/\n$/, "")}
+							
+							${chalk.bold("Next steps:")}${
+								tsConfigStatus === SaveFileStatus.CANCELLED 
+									? "\n\t\t\t\t\t\t\t  • Update your existing tsconfig.json to extend aliases.tsconfig.json" 
+									: ""
+							}
+							  • You can now import modules using aliases like: import { Button } from 'ui.buttons';
+							  • Run TypeScript/JS compilation: ${chalk.green('chef build')};
+							  • More info: ${chalk.green('chef build --help')};
+						`;
 
-						let message = '';
-
-						message += chalk.bold('Files created:\n');
-						message +=
-							'  • ' + chalk.yellow('aliases.tsconfig.json') +
-							` — Contains ${aliasesCount} path aliases for easier imports\n`;
-
-						if (tsConfigCreated)
-						{
-							message +=
-								'  • ' + chalk.yellow('tsconfig.json') +
-								` — Main config that extends aliases for TypeScript compilation\n`;
-						}
-						message += '\n';
-
-						message += chalk.bold('What these files do:\n');
-						message +=
-							'  • ' +
-							chalk.cyan('aliases.tsconfig.json') +
-							' — Maps extension names to their source paths (e.g. "@ui/button" → "./src/ui/button")\n';
-						if (tsConfigCreated)
-						{
-							message +=
-								'  • ' +
-								chalk.cyan('tsconfig.json') +
-								' — Includes the aliases and sets up TypeScript compiler options\n';
-						}
-						message += '\n';
-
-						message += chalk.bold('Next steps:\n');
-						message += '  • You can now import modules using aliases like: import Button from "@ui/button"\n';
-						message += '  • Run TypeScript compilation: chef build\n';
-						message += '  • More info: chef build --help\n\n';
-
-						message += chalk.bold.blue(`💡 Tip:\n`);
-						message += `These configs help TypeScript resolve imports faster and make refactoring easier.\n`;
-
+						console.log('');
 						console.log(
-							'\n' +
-							boxen(message, {
-								padding: 1,
-								borderStyle: 'round',
-								borderColor: 'green',
-								title: chalk.bold.green(
-									`✓ TypeScript Environment Configured Successfully!`,
-								),
-							}),
+							boxen(
+								message,
+								{
+									title: chalk.bold.green(`✓ TypeScript Environment ${titleStatus} Configured!`),
+									padding: 1,
+									borderStyle: 'round',
+									borderColor: 'yellow',
+								},
+							),
 						);
 
 						resolve();
@@ -373,7 +343,7 @@ const initTSCommand = new Command('ts')
 const initCommand = new Command('init')
 	.description('Initialize environment')
 	.addOption(pathOption)
-	.action(async (subcommand, args) => {
+	.action(async () => {
 		await initTestsCommand.parseAsync([]);
 		console.log('\n');
 		await initTSCommand.parseAsync([]);
